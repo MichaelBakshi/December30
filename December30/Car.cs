7cc15f5989e8c84cb56911fcd96d4c9648d58073
MichaelBakshi <62958898+MichaelBakshi@users.noreply.github.com>
2021-01-08 19:57:08 +0200

    Add files via upload

---
+++ December30/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace December30
+{
+    class Car
+    {
+        public Int64 ID { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public Int64 Year { get; set; }
+        public Car()
+        {
+
+        }
+
+        public Car(string manufacturer, string model, int year )
+        {
+            Manufacturer = manufacturer;
+            Model = model;
+            Year = year;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Car: {ID} {Manufacturer} {Model} {Year} ";
+        }
+    }
+}
---
+++ December30/CarsDAO.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace December30
+{
+    static class CarsDAO
+    {
+        public static void GetAllCars()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(@"Data Source = C:\\USERS\\MICHAEL\\DOCUMENTS\\December30Test.db; Version = 3;"))
+            {
+                conn.Open();
+
+                using (SQLiteCommand select_query = new SQLiteCommand("SELECT * from Cars", conn))
+                {
+                    using (SQLiteDataReader reader = select_query.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader["ID"]}, {reader["Manufacturer"]}, {reader["Model"]}, {reader["Year"]}");
+                        }
+                    }
+                }
+            }
+        }
+
+        static int ExecuteNonQuery(string query)
+        {
+            int result = 0;
+
+            using (SQLiteCommand cmd = new SQLiteCommand())
+            {
+                using (cmd.Connection = new SQLiteConnection(@"Data Source = C:\\USERS\\MICHAEL\\DOCUMENTS\\December30Test.db; Version = 3;"))
+                {
+                    cmd.Connection.Open();
+
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.CommandText = query;
+
+                    result = cmd.ExecuteNonQuery();
+                }
+            }
+
+            return result;
+        }
+        public static void AddCar(Car c)
+        {
+            ExecuteNonQuery("INSERT INTO CARS(Manufacturer, Model, Year)" +
+            $"VALUES('{c.Manufacturer}', '{c.Model}', {c.Year});");
+
+        }
+
+        public static void UpdateCar(Car c, int id)
+        {
+            int result = ExecuteNonQuery(
+                $"UPDATE CARS SET Manufacturer='{c.Manufacturer}', Model='{c.Model}', Year={c.Year} " +
+                $"WHERE ID={id}");
+        }
+
+        public static List<Car> SelectCarsByManufacturer(string manufacturer)
+        {
+            List<Car> listOfCars = new List<Car>();
+            using (SQLiteCommand cmd = new SQLiteCommand())
+            {
+                using (cmd.Connection = new SQLiteConnection(@"Data Source = C:\\USERS\\MICHAEL\\DOCUMENTS\\December30Test.db; Version = 3;"))
+                {
+                    cmd.Connection.Open();
+
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.CommandText = $"Select * from Cars WHERE Manufacturer='{manufacturer}'";
+
+                    SQLiteDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Car c = new Car
+                        {
+                            ID = (Int64)reader["ID"],
+                            Manufacturer = reader["Manufacturer"].ToString(),
+                            Model = reader["Model"].ToString(),
+                            Year = (Int64)reader["Year"]
+                        };
+
+                        listOfCars.Add(c);
+                    }
+
+                }
+            }
+
+            return listOfCars;
+        }
+
+        public static void GetAllCarsWithTests()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(@"Data Source = C:\\USERS\\MICHAEL\\DOCUMENTS\\December30Test.db; Version = 3;"))
+            {
+                conn.Open();
+
+                using (SQLiteCommand select_query = new SQLiteCommand("SELECT * from Cars JOIN Tests on Cars.ID=Tests.Car_ID", conn))
+                {
+                    using (SQLiteDataReader reader = select_query.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader["ID"]}, {reader["Manufacturer"]}, {reader["Model"]}, {reader["Year"]}, {reader["Car_ID"]}, {reader["IsPassed"]}, {reader["Date"]}");
+                        }
+                    }
+                }
+            }
+        }
+
+        public static  int RemoveCar(int id)
+        {
+            int result = ExecuteNonQuery($"DELETE FROM CARS WHERE ID={id}");
+            return result;
+        }
+    }
+}
